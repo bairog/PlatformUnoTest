cf70416acf5657b4c8aa3d732ff11721f1cc2b22
Leo <Leo@topaz1.local>
2022-11-02 11:06:11 +0300

    забыл файл

---
+++ PlatformUnoTest/PlatformUnoTest.Shared/EmployeeListViewModel .cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using PlatformUnoTest.DAL;
+
+namespace PlatformUnoTest
+{
+    public class EmployeeListViewModel
+    {
+        private ObservableCollection<Employee> _employee = new ObservableCollection<Employee>();
+        public ObservableCollection<Employee> Employee
+        {
+            get { return _employee; }
+            set { _employee = value; }
+        }
+
+
+        public EmployeeListViewModel()
+        {
+            Employee = new ObservableCollection<Employee>(StorageSystem.GetAllEmployee());
+        }
+    }
+}
